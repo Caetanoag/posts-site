2390f970f73e5c4597b8eddf8a4247d1ea9c23b1
caetano <2025311434@aluno.riogrande.ifrs.edu.br>
2026-08-28 21:59:30 -0300

    Added user end points

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
+import type { Request, Response, NextFunction } from "express";
 import express from "express";
 import postsRoutes from "./routes/posts.routes";
-
+import userRoutes from "./routes/user.routes";
 export const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -15,4 +16,5 @@ app.use((req, res, next) => {
 });
 
 app.use("/posts", postsRoutes);
+app.use("/users", userRoutes);
 export default app;
